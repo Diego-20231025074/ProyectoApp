de1c7c5f7a5c1b06b35333239c75836077cc0f04
Diego Alejandro Garcia <yeyediego1200@gmail.com>
2025-06-24 12:23:53 -0500

    v 0.2

---
+++ programa.js
@@ -0,0 +1,12 @@
+var map = L.map('map').setView([4.628199552170864, -74.06569003375023], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function onMapClick(e) {
+     var marker = L.marker(e.latlng).addTo(map);
+}
+
+map.on('click', onMapClick);
